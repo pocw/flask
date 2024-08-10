56149fdcb250d6964353aaf019b8a5797df565db
pocw <115600683+pocw@users.noreply.github.com>
2024-08-10 10:25:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-from flask import Flask, jsonify
 from flask import Flask, Response
 import time
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def stream():
     def generate():
         while True:
             # Generate a string with 10,000 characters
-            large_text = 'A' * 100000
+            large_text = 'A' * 10000
             # Send the data to the client
             yield f"data: {large_text}\n\n"
             # Wait for 0.5 seconds
